786c36d94c3f0624d6050ad2f7800d919eb18eb7
SanadHantash <sanadhantash24@gmail.com>
2024-02-21 05:18:33 +0300

    babysitter detail provides care of age section

--- client/src/Pages/BabySitterdetail.jsx
+++ client/src/Pages/BabySitterdetail.jsx
@@ -2,73 +2,224 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 
-
+import months from "../Assets/0-12months.svg";
+import years1_2 from "../Assets/1-2years.svg";
+import years2_3 from "../Assets/1-2years.svg";
+import years3_5 from "../Assets/2-3years.svg";
+import years5 from "../Assets/5+years.svg";
+import not0_12 from "../Assets/0-12months(not).svg"
+import not1_2 from "../Assets/1-2years(not).svg"
+import not2_3 from "../Assets/2-3years(not).svg"
+import not3_5 from "../Assets/3-5years(not).svg"
+import not5 from "../Assets/5+years(not).svg"
 
 
 function BabySitterdetail() {
-    const [babysitterDetails, setBabysitterDetails] = useState([]);
-    const { id } = useParams();
-    useEffect(() => {
-        const fetchBabySitterDetails = async () => {
-          try {
-           
-            const response = await axios.get(
-              `http://localhost:8080/sitter/detail/${id}`
-            );
-            setBabysitterDetails(response.data);
-            console.log("Sitter Details API response:", response.data);
-          } catch (error) {
-            console.error("Error fetching sitter details:", error.response);
-          }
-        };
-    
-        fetchBabySitterDetails();
-      }, [id]);
+  const [babysitterDetails, setBabysitterDetails] = useState([]);
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchBabySitterDetails = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:8080/sitter/detail/${id}`
+        );
+        setBabysitterDetails(response.data);
+        console.log("Sitter Details API response:", response.data);
+      } catch (error) {
+        console.error("Error fetching sitter details:", error.response);
+      }
+    };
+
+    fetchBabySitterDetails();
+  }, [id]);
   return (
-    <div className="container">
-      <div className="grid grid-cols-2 gap-4">
-    <div className="flex items-center">
-        <img 
-            className="float-none rounded-full mt-20 ml-10"    
+    <>
+      <div class="w-full max-w-7xl px-4 mx-auto sm:px-8 mt-24">
+        <blockquote class="relative grid items-center bg-gradient-to-b from-[#FF90BC] to-[#F9F9E0] shadow-xl md:grid-cols-3 rounded-xl">
+          <img
+            class="hidden object-cover w-full h-full rounded-l-xl md:block"
+            style={{ clipPath: "polygon(0 0%, 100% 0%, 75% 100%, 0% 100%)" }}
             src={
-                babysitterDetails.sitter &&
-                babysitterDetails.sitter.length > 0 &&
-                babysitterDetails.sitter[0].image
-                    ? babysitterDetails.sitter[0].image
-                    : "placeholder_image_url_or_default"
+              babysitterDetails.sitter &&
+              babysitterDetails.sitter.length > 0 &&
+              babysitterDetails.sitter[0].image
+                ? babysitterDetails.sitter[0].image
+                : "placeholder_image_url_or_default"
             }
-        />
-        <div className="ml-4">
-            <div className="mb-4 bg-opacity-60 text-2xl mix-blend-screen px-4">
-                {babysitterDetails.sitter && babysitterDetails.sitter[0].user_name ? babysitterDetails.sitter[0].user_name : "Username Not Available"}
+            alt="Image"
+          />
+
+          <article class="relative p-6 md:p-8 md:col-span-2">
+            <div class="space-y-8">
+              <p class="text-base sm:leading-relaxed md:text-base">
+                {babysitterDetails.sitter &&
+                babysitterDetails.sitter[0].description
+                  ? babysitterDetails.sitter[0].description
+                  : "Description Not Available"}
+              </p>
+
+              <footer class="flex items-center space-x-4 md:space-x-0">
+                <span class="font-extrabold text-2xl text-[#FF90BC]">
+                  {" "}
+                  {babysitterDetails.sitter &&
+                  babysitterDetails.sitter[0].user_name
+                    ? babysitterDetails.sitter[0].user_name
+                    : "Description Not Available"}
+                </span>
+              </footer>
             </div>
-            <div className="mb-4 bg-opacity-60 text-2xl mix-blend-screen px-4">
-                Experience: {babysitterDetails.sitter && babysitterDetails.sitter[0].experience_level ? babysitterDetails.sitter[0].experience_level : "Experience level Not Available"}
+          </article>
+        </blockquote>
+      </div>
+
+      <section class="text-gray-700 body-font">
+        <div class="container px-4 py-20 mx-auto">
+          <div class="flex flex-wrap -m-4 text-center">
+            <div class="p-2 md:w-1/5 sm:w-1/5 w-full">
+              <div class="border-2 bg-[#F9F9E0] border-gray-600 px-2 py-4 rounded-lg transform transition duration-500 hover:scale-110">
+                {babysitterDetails.sitter &&
+                babysitterDetails.sitter[0]["0_12_months"] ? (
+                  <>
+                    <img
+                      src={months}
+                      alt="months Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#FF90BC]">
+                      0-12 months
+                    </h2>
+                  </>
+                ) : (
+                    <>
+                    <img
+                      src={not0_12}
+                      alt="years1-2 Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#C9C9C9]">
+                      0-12 months
+                    </h2>
+                  </>
+                )}
+              </div>
             </div>
+            <div class="p-2 md:w-1/5 sm:w-1/5 w-full">
+            <div class="border-2 bg-[#F9F9E0] border-gray-600 px-2 py-4 rounded-lg transform transition duration-500 hover:scale-110">
+                {babysitterDetails.sitter &&
+                babysitterDetails.sitter[0]["1_2_years"] ? (
+                  <>
+                    <img
+                      src={years1_2}
+                      alt="years1-2 Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#FF90BC]">
+                      1-2 years
+                    </h2>
+                  </>
+                ) : (
+                    <>
+                    <img
+                      src={not1_2}
+                      alt="years1-2 Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#C9C9C9]">
+                      1-2 years
+                    </h2>
+                  </>
+                )}
+              </div>
+            </div>
+            <div class="p-2 md:w-1/5 sm:w-1/5 w-full">
+            <div class="border-2 bg-[#F9F9E0] border-gray-600 px-2 py-4 rounded-lg transform transition duration-500 hover:scale-110">
+                {babysitterDetails.sitter &&
+                babysitterDetails.sitter[0]["2_3_years"] ? (
+                  <>
+                    <img
+                      src={years2_3}
+                      alt="years2-3 Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#FF90BC]">
+                      2-3 years
+                    </h2>
+                  </>
+                ) : (
+                    <>
+                    <img
+                      src={not2_3}
+                      alt="years1-2 Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#C9C9C9]">
+                      2-3 years
+                    </h2>
+                  </>
+                )}
+              </div>
+            </div>
+            <div class="p-2 md:w-1/5 sm:w-1/5 w-full">
+            <div class="border-2 bg-[#F9F9E0] border-gray-600 px-2 py-4 rounded-lg transform transition duration-500 hover:scale-110">
+                {babysitterDetails.sitter &&
+                babysitterDetails.sitter[0]["3_5_years"] ? (
+                  <>
+                    <img
+                      src={years3_5}
+                      alt="years3-5 Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#FF90BC]">
+                      3-5 months
+                    </h2>
+                  </>
+                ) : (
+                    <>
+                    <img
+                      src={not3_5}
+                      alt="years1-2 Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#C9C9C9]">
+                      2-3 years
+                    </h2>
+                  </>
+                )}
+              </div>
+            </div>
+            <div class="p-2 md:w-1/5 sm:w-1/5 w-full">
+            <div class="border-2 bg-[#F9F9E0] border-gray-600 px-2 py-4 rounded-lg transform transition duration-500 hover:scale-110">
+                {babysitterDetails.sitter &&
+                babysitterDetails.sitter[0]["3_5_years"] ? (
+                  <>
+                    <img
+                      src={years5}
+                      alt="+5years Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#FF90BC]">
+                     +5 years
+                    </h2>
+                  </>
+                ) : (
+                    <>
+                    <img
+                      src={not5}
+                      alt="years1-2 Icon"
+                      class="w-20 h-20 inline-block mr-2"
+                    />
+                    <h2 class="title-font font-medium text-2xl text-[#C9C9C9]">
+                     +5 years
+                    </h2>
+                  </>
+                )}
+              </div>
+            </div>
+          </div>
         </div>
-    </div>
-    <div className="flex items-center">
-        <div className="ml-4">
-            <div className="mb-4 bg-opacity-60 text-2xl mix-blend-screen px-4">
-                Education: {babysitterDetails.sitter && babysitterDetails.sitter[0].education ? babysitterDetails.sitter[0].education : "Education Not Available"}
-            </div>
-            <div className="mb-4 bg-opacity-60 text-2xl mix-blend-screen px-4">
-                Rate: {babysitterDetails.sitter && babysitterDetails.sitter[0].rate ? babysitterDetails.sitter[0].rate : "Rate Not Available"}
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
- 
- <p className="text-gray-600 mb-4 m-10 "> {babysitterDetails.sitter && babysitterDetails.sitter[0].description
-                ? babysitterDetails.sitter[0].description
-                : "Description Not Available"}</p>
-
-</div>
-
-  )
+      </section>
+    </>
+  );
 }
 
-export default BabySitterdetail
+export default BabySitterdetail;
--- client/src/Pages/BabySitters.jsx
+++ client/src/Pages/BabySitters.jsx
@@ -2,59 +2,57 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 function BabySitters() {
-    const[sitters,setSitters]=useState([])
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get(
-              "http://localhost:8080/sitters"
-            );
-            setSitters(response.data);
-          } catch (error) {
-            console.error("Error fetching data:", error);
-          }
-        };
-    
-        fetchData();
-      }, []);
+  const [sitters, setSitters] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/sitters");
+        setSitters(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-
+    fetchData();
+  }, []);
 
   return (
-<>
-
-{sitters && sitters.sitters ? (
-  <div className="mt-20 my-8 lg:my-10 lg:mt-20 container px-6 mx-auto md:flex-row items-start md:items-center justify-between pb-4 border-b border-gray-300 ">
-    {sitters.sitters.map((sitter, index) => (
-      <div key={sitter.id} className="card w-96 glass inline-block bg-gradient-to-r from-[#FFC0D9] to-[#F9F9E0] m-10 p-10 w-18p">
-          <figure>
-          <img className="block m-auto" src={sitter.image} alt={sitter.user_name}  />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title text-[#FF90BC] font-bold">{sitter.user_name}</h2>
-          <p >Experience: {sitter.experience_level}</p>
-          <div className="card-actions justify-end">
-          <button className="w-100 block mx-auto rounded-full bg-[#FF90BC] hover:shadow-lg font-semibold text-white px-6 py-2">show more</button>
-          </div>
+    <>
+      {sitters && sitters.sitters ? (
+        <div className="mt-20 my-8 lg:my-10 lg:mt-20 container px-6 mx-auto md:flex-row items-start md:items-center justify-between pb-4 border-b border-gray-300 ">
+          {sitters.sitters.map((sitter, index) => (
+            <div
+              key={sitter.id}
+              className="card w-96 glass inline-block bg-gradient-to-r from-[#FFC0D9] to-[#F9F9E0] m-10 p-10 w-18p"
+            >
+              <figure>
+                <img
+                  className="block m-auto"
+                  src={sitter.image}
+                  alt={sitter.user_name}
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-[#FF90BC] font-bold">
+                  {sitter.user_name}
+                </h2>
+                <p>Experience: {sitter.experience_level}</p>
+                <div className="card-actions justify-end">
+                  <Link to={`/babysitterDetails/${sitter.id}`}>
+                    <button className="w-1/2 block mx-auto rounded-full bg-[#FF90BC] hover:shadow-lg font-semibold text-white px-6 py-2">
+                      show more
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-       
-      </div>
-    ))}
-  </div>
-) : (
-  <p>Loading...</p>
-)}
-</>
-
-
-
-
-
-
-
-  
-   
-  )
+      ) : (
+        <p>Loading...</p>
+      )}
+    </>
+  );
 }
 
-export default BabySitters
+export default BabySitters;
